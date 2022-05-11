e18d4498760852979a0ecb6c9d0fc8c689140df7
Nishant Bansal <nishant.1923it1002@kiet.edu>
2022-05-11 21:17:11 +0530

    Create Largest number with given sum.java

---
+++ Largest number with given sum.java
@@ -0,0 +1,38 @@
+/*
+Link: "https://practice.geeksforgeeks.org/problems/largest-number-with-given-sum-1587115620/1#"
+Largest number with given sum 
+Easy 
+Geek lost the password of his super locker. He remembers the number of digits N as well as the sum S of all the digits of his password.
+He know that his password is the largest number of N digits that can be made with given sum S. As he is busy doing his homework, help him retrieving his password.
+
+Example 1:
+
+Input:
+  N = 5, S = 12
+Output: 
+  93000
+Explanation:
+Sum of elements is 12. Largest possible 5 digit number is 93000 with sum 12.
+*/
+class Solution
+{
+    //Function to return the largest possible number of n digits
+    //with sum equal to given sum.
+    static String largestNumber(int n, int sum)
+    {
+        if(sum <= (9*n)){
+            String ans = "";
+            int i = 9;
+            while(sum > 0){
+                if(sum-i >= 0){
+                    ans += i;
+                    sum -= i;
+                }
+                else i--;
+            }
+            while(n > ans.length()) ans += 0;
+            return ans;
+        }
+        return "-1";
+    }
+}
